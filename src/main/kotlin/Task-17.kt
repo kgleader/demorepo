a69ae3012f325b6780d2ba833893dd0177c5b231
Meerim <44379294+kgleader@users.noreply.github.com>
2021-03-13 23:04:23 +0600

    percentage of bank for three years

---
+++ src/main/kotlin/Task-17.kt
@@ -0,0 +1,14 @@
+import jdk.jfr.Percentage
+import kotlin.math.pow
+
+fun main() {
+	val money = readLine()!!.toInt()
+	val percent = readLine()!!.toInt()
+	val otvet = percentInThreeYears(money, percent)
+	println(otvet)
+}
+
+fun percentInThreeYears(initial: Int, percent: Int): Double {
+	val sum = initial * (1 + percent / 100.toDouble()).pow(3)
+	return sum
+}
